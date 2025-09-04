460dc6d55910cc471f9ee5bf8aa168b4300e7e46
Young Caesar <YoungCaesar63@gmail.com>
2025-09-04 19:28:45 +0000

    نسق شاشة عرض التقويم

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -33,8 +33,8 @@ function Calendar({
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
         ),
-        nav_button_previous: "absolute left-1",
-        nav_button_next: "absolute right-1",
+        nav_button_previous: "absolute right-1",
+        nav_button_next: "absolute left-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
@@ -58,12 +58,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
+        IconLeft: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
       }}
       captionLayout="dropdown-buttons"
       fromYear={1950}
@@ -75,5 +71,3 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
-
-    
