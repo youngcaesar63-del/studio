378ee68bd2a6a81b2ca2931d178dc812acfd46a9
Young Caesar <YoungCaesar63@gmail.com>
2025-09-04 21:23:55 +0000

    لم تضف الخط

--- src/app/dashboard/reports/page.tsx
+++ src/app/dashboard/reports/page.tsx
@@ -189,7 +189,7 @@ export default function ReportsPage() {
 
       printWindow.document.write('<style>');
       printWindow.document.write(`
-        @import url('https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&family=Tajawal:wght@400;500;700&family=PT+Serif:wght@700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&family=PT+Serif:wght@700&family=Tajawal:wght@400;500;700&display=swap');
         @page {
             size: A4 ${pageOrientation};
             margin: 1.5cm;
