e8e74e6325f2300051f080a84844824b8194d030
Young Caesar <YoungCaesar63@gmail.com>
2025-09-05 19:43:52 +0000

    هل تحتاج هذة القاعدة للاتصال بالانترنت

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+'use client';
+
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  "apiKey": "YOUR_API_KEY",
+  "authDomain": "YOUR_AUTH_DOMAIN",
+  "projectId": "YOUR_PROJECT_ID",
+  "storageBucket": "YOUR_STORAGE_BUCKET",
+  "messagingSenderId": "YOUR_MESSAGING_SENDER_ID",
+  "appId": "YOUR_APP_ID"
+};
+
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+}
+
+const db = getFirestore(app);
+
+export { app, db };
