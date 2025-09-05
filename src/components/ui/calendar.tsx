9b764e7fed5d8de3a038b183b7d13cb9941ec78d
Young Caesar <YoungCaesar63@gmail.com>
2025-09-05 19:06:30 +0000

    لم تظهر القائمة المنسدلة للسنين و الشهور في اخنيار التاريخ

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -26,15 +26,15 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium hidden",
+        caption_label: "text-sm font-medium",
         caption_dropdowns: "flex justify-center gap-2",
-        nav: "space-x-1 flex items-center",
+        nav: "space-x-1 flex items-center absolute",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
         ),
-        nav_button_previous: "absolute right-1",
-        nav_button_next: "absolute left-1",
+        nav_button_previous: "right-1.5",
+        nav_button_next: "left-1.5",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
