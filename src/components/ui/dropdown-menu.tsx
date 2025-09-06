6d1afd68d3c9d1ae98bcb6e5b9bab0cfdfac2bfa
Young Caesar <YoungCaesar63@gmail.com>
2025-09-06 14:51:35 +0000

    حول الايقونة لليمين (_for element span_)

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -174,7 +175,7 @@ const DropdownMenuShortcut = ({
 }: React.HTMLAttributes<HTMLSpanElement>) => {
   return (
     <span
-      className={cn("ml-auto text-xs tracking-widest opacity-60", className)}
+      className={cn("mr-auto text-xs tracking-widest opacity-60", className)}
       {...props}
     />
   )
