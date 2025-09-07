d50ebd77c132824a98cef428f6e0e224bc3d29ad
Young Caesar <YoungCaesar63@gmail.com>
2025-09-07 22:14:43 +0000

    Try fixing this error: `Console Error: `value` prop on `input` should no

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -14,6 +14,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         )}
         ref={ref}
         {...props}
+        value={props.value ?? ""}
       />
     )
   }
