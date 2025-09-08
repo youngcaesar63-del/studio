d4108e70e131a8ed5e871a5b0b4b7b6b645caa64
Young Caesar <YoungCaesar63@gmail.com>
2025-09-08 16:03:55 +0000

    راجع بحثا عن اخطا

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -14,7 +14,6 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         )}
         ref={ref}
         {...props}
-        value={props.value ?? ""}
       />
     )
   }
